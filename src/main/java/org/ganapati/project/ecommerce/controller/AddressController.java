62c9c0a2e4d2000e4897706a63f144a7f1ed869b
Ganapati Jarali <ganapatijarali4126@gmail.com>
2026-04-14 22:12:19 +0530

    added the validation on the addressController.

--- src/main/java/org/ganapati/project/ecommerce/controller/AddressController.java
+++ src/main/java/org/ganapati/project/ecommerce/controller/AddressController.java
@@ -1,5 +1,8 @@
 package org.ganapati.project.ecommerce.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.ganapati.project.ecommerce.common.BaseResponse;
 import org.ganapati.project.ecommerce.dto.AddressRequest;
@@ -8,38 +11,39 @@ import org.ganapati.project.ecommerce.dto.PageResponse;
 import org.ganapati.project.ecommerce.service.AddressService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/address")
 @RequiredArgsConstructor
+@Validated
 public class AddressController {
 
     private final AddressService addressService;
 
     // Add address for user
     @PostMapping
-    public ResponseEntity<AddressResponse> addAddress(
-            @RequestBody AddressRequest request
+    public ResponseEntity<AddressResponse> addAddress(@RequestBody @Valid AddressRequest request
     ) {
         return new ResponseEntity<>(addressService.addAddress(request), HttpStatus.CREATED);
     }
 
     // Get all addresses of user
     @GetMapping
-    public ResponseEntity<BaseResponse<PageResponse<AddressResponse>>> fetchAllAddressesByUser(@RequestParam(name = "page") int page, @RequestParam(name = "size") int size) {
+    public ResponseEntity<BaseResponse<PageResponse<AddressResponse>>> fetchAllAddressesByUser(@RequestParam(name = "page") @Min(value = 0, message = "Invalid page Number") int page, @RequestParam(name = "size") @Min(value = 1, message = "Page size at least 1") int size) {
 
         return ResponseEntity.ok(addressService.getAddressesByUser(page, size));
     }
-    //TODO new api for the getAddressByAddressById.
+
 
     @GetMapping("/{addressId}")
-    public ResponseEntity<BaseResponse<AddressResponse>> fetchAddressByAddressById(@PathVariable(name = "addressId") Long addressId) {
+    public ResponseEntity<BaseResponse<AddressResponse>> fetchAddressByAddressById(@PathVariable(name = "addressId") @Min(value = 1, message = "AddressId Starts With One") Long addressId) {
         return ResponseEntity.ok(addressService.getAddressByAddressById(addressId));
     }
 
     @PutMapping("/{addressId}")
-    public ResponseEntity<BaseResponse> updateAddress(@PathVariable("addressId") Long addressId, @RequestBody AddressRequest addressRequest) {
+    public ResponseEntity<BaseResponse> updateAddress(@PathVariable("addressId") @Min(value = 1, message = "AddressId Starts With One") Long addressId, @RequestBody AddressRequest addressRequest) {
         return ResponseEntity.ok(addressService.updateAddress(addressRequest, addressId));
     }
 
--- src/main/java/org/ganapati/project/ecommerce/dto/AddressRequest.java
+++ src/main/java/org/ganapati/project/ecommerce/dto/AddressRequest.java
@@ -1,18 +1,29 @@
 package org.ganapati.project.ecommerce.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.ganapati.project.ecommerce.common.ModelErrorConstant;
 import org.ganapati.project.ecommerce.enums.AddressType;
 
 @Data
 public class AddressRequest {
-    private String fullName;
+    @NotBlank(message = ModelErrorConstant.MOBILE_NUMBER_MANDATORY)
+    @Pattern(regexp = "^[0-9]{10}$", message = "Invalid Mobile Number")
     private String mobileNo;
+    @NotBlank(message = ModelErrorConstant.ADDRESS1_MANDATORY)
     private String addressLine1;
     private String addressLine2;
+    @NotBlank(message = ModelErrorConstant.CITY_MANDATORY)
     private String city;
+    @NotBlank(message = ModelErrorConstant.STATE_MANDATORY)
     private String state;
+    @NotBlank(message = ModelErrorConstant.COUNTRY_MANDATORY)
     private String country;
+    @NotBlank(message = ModelErrorConstant.PIN_CODE_MANDATORY)
+    @Pattern(regexp = "^[1-9][0-9]{5}$", message = "Invalid PinCode Number")
     private String pinCode;
-    private AddressType addressType;
+    @Pattern(regexp = "SHIPPING|BILLING", message = "Only SHIPPING or BILLING allowed")
+    private String addressType;
     private Boolean defaultAddress; //null //false
 }
